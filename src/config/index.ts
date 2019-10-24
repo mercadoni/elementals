901650f8768f6de230681fd4dd6a0f8242c88d6e
Carlos Rodriguez <cerodriguezl@gmail.com>
2019-10-24 15:47:40 -0500

    Environment variable to choose if you want lowercased env variables or not

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,8 @@
 import nconf from 'nconf'
 import * as path from 'path'
 
-nconf.argv().env({ separator: '__', parseValues: true, lowerCase: true })
+const lowerCase = process.env.LOWER_CASE_ENV_VARS !== 'false'
+nconf.argv().env({ separator: '__', parseValues: true, lowerCase })
 nconf.defaults({ conf: path.resolve('config.json') })
 nconf.file(nconf.get('conf'))
 
