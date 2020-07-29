4fcb4a93abe6dd5560cd93d27c671d9d865526e0
Ana Maria R <anmrdz@gmail.com>
2020-07-29 18:25:33 -0500

    Add encryption service

---
+++ src/encryption/index.ts
@@ -0,0 +1,62 @@
+
+import Logger from '../logger'
+import config from '../config'
+import * as AWS from '@aws-crypto/client-node'
+
+export declare type EncryptionContext = {
+  [index: string]: string;
+};
+
+const logger = Logger('encryption')
+
+interface Encryption {
+  encrypt: (plaintext: string, context: EncryptionContext) => Promise<string>
+  decrypt: (ciphertext: string, context: EncryptionContext) => Promise<string>
+}
+
+const newKeyring = (conf: any): AWS.KeyringNode => {
+  if (!conf || !conf.generatorKeyId || !conf.keyIds) {
+    const error = new Error('INVALID_ENCRYPTION_CONFIG')
+    logger.error('Verify AWS Encryption keys configuration', {}, error)
+    throw error
+  }
+
+  const generatorKeyId = conf.generatorKeyId
+  const keyIds = conf.keyIds
+  const keyring = new AWS.KmsKeyringNode({ generatorKeyId, keyIds })
+  return keyring
+}
+
+const encryption = (configName: string): Encryption => {
+  const conf = config.get(configName)
+  const keyring = newKeyring(conf)
+
+  const encrypt = async (plaintext: string, context: EncryptionContext): Promise<string> => {
+    const { result } = await AWS.encrypt(keyring, plaintext, { encryptionContext: context })
+    return result.toString('base64')
+  }
+
+  const decrypt = async (ciphertext: string, context: EncryptionContext) : Promise<string> => {
+    const buff = Buffer.from(ciphertext, 'base64')
+    const { plaintext, messageHeader } = await AWS.decrypt(keyring, buff)
+    const { encryptionContext } = messageHeader
+
+    Object.entries(context).forEach(([key, value]) => {
+      if (encryptionContext[key] !== value) {
+        const error = new Error('Encryption Context does not match expected values')
+        logger.error('Encrypted message is corrupted or untrustworthy', context, error)
+        throw error
+      }
+    })
+
+    return plaintext.toString()
+  }
+
+  return {
+    encrypt,
+    decrypt
+  }
+}
+
+export type { Encryption }
+export default encryption
