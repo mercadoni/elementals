a36ddc370d0d5809b2c647f3484c3a2d538f25c5
David Felipe Camargo Polo <davidcp90@gmail.com>
2020-08-05 21:55:41 -0500

    Fixing hasp and cryptic (#8)
    * fixed modules export for hasp and cryptic
    
    * updated package lock
    
    * Version 0.7.0
    
    * change encrypt return
    
    * Version 0.7.1-alpha.0
    
    * Version 0.7.1

--- src/cryptic/index.ts
+++ src/cryptic/index.ts
@@ -1,20 +1,22 @@
 const crypto = require('crypto')
-const algorithm = 'aes-256-cbc'
-const iv = crypto.randomBytes(16)
 
 interface CrypticResponse {
   iv: string,
   encryptedData: string
 }
 
-const encrypt = (key: string, rawValue: object): CrypticResponse => {
+export const encrypt = (algorithm: string, key: string, rawValue: object): CrypticResponse => {
+  const iv = crypto.randomBytes(16)
   const cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv)
   let encrypted = cipher.update(JSON.stringify(rawValue))
   encrypted = Buffer.concat([encrypted, cipher.final()])
-  return { iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') }
+  return {
+    iv: iv.toString('hex'),
+    encryptedData: encrypted.toString('hex')
+  }
 }
 
-const decrypt = (key: string, rawValue: CrypticResponse): object => {
+export const decrypt = (algorithm: string, key: string, rawValue: CrypticResponse): object => {
   const iv = Buffer.from(rawValue.iv, 'hex')
   const encryptedText = Buffer.from(rawValue.encryptedData, 'hex')
   const decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), iv)
@@ -22,8 +24,3 @@ const decrypt = (key: string, rawValue: CrypticResponse): object => {
   decrypted = Buffer.concat([decrypted, decipher.final()])
   return JSON.parse(decrypted.toString())
 }
-
-export default [
-  encrypt,
-  decrypt
-]
