d4288f145e7f5ce146191d4a7d1225d2828760ab
Carlos Rodriguez <carlos.rodriguez@instaleap.io>
2020-12-17 17:10:37 -0500

    Improve RabbitMQ metrics
    This commit restores the incoming and failed message counters, to
    initialize them to zero when the application starts and avoid missing
    metrics. Also, the histogram that tracks message lifetimes was splitted
    in two: one that tracks how long messages remain queued and another one
    that tracks how long does it takes consumers to process the messages.

--- src/rabbitmq/index.ts
+++ src/rabbitmq/index.ts
@@ -1,51 +1,71 @@
 import amqp from 'amqp-connection-manager'
-import { ConfirmChannel, ConsumeMessage, Options } from 'amqplib'
-import { Counter } from 'prom-client'
+import { ConfirmChannel, ConsumeMessage, Options, MessageProperties } from 'amqplib'
+import { Counter, Histogram } from 'prom-client'
 import Logger from '../logger'
 import config from '../config'
 
 const logger = Logger('rabbitmq')
 
 const totalIncomingMessages = new Counter({
-  name: 'elementals_amqp_incoming_total',
+  name: 'elementals_rabbitmq_incoming_total',
   help: 'Messages processed',
   labelNames: ['queue']
 })
 
 const failedIncomingMessages = new Counter({
-  name: 'elementals_amqp_incoming_errors_total',
+  name: 'elementals_rabbitmq_incoming_errors_total',
   help: 'Errors found',
   labelNames: ['queue']
 })
 
+const initializeQueueMetrics = (queue: string) => {
+  totalIncomingMessages.inc({ queue }, 0)
+  failedIncomingMessages.inc({ queue }, 0)
+}
+
 const totalOutgoingMessages = new Counter({
-  name: 'elementals_amqp_outgoing_total',
+  name: 'elementals_rabbitmq_outgoing_total',
   help: 'Messages produced',
-  labelNames: ['exchange', 'routingKey']
+  labelNames: ['exchange']
 })
 
 const failedOutgoingMessages = new Counter({
-  name: 'elementals_amqp_outgoing_errors_total',
+  name: 'elementals_rabbitmq_outgoing_errors_total',
   help: 'Errors found',
-  labelNames: ['exchange', 'routingKey']
+  labelNames: ['exchange']
 })
 
-const countIncomingMessage = (queue: string) => {
-  totalIncomingMessages.inc({ queue }, 1, Date.now())
-  failedIncomingMessages.inc({ queue }, 0, Date.now()) // Sounds silly to increment by 0, but this initializes the metric
+const countIncomingMessage = (queue: string, properties: MessageProperties) => {
+  totalIncomingMessages.inc({ queue }, 1)
+  const time = properties.timestamp ? Date.now() - properties.timestamp : 0
+  waitingDuration.observe({ queue }, time / 1000)
 }
 
 const countIncomingError = (queue: string) => {
-  failedIncomingMessages.inc({ queue }, 1, Date.now())
+  failedIncomingMessages.inc({ queue }, 1)
 }
 
-const countOutgoingMessage = (exchange: string, routingKey: string) => {
-  totalOutgoingMessages.inc({ exchange, routingKey }, 1, Date.now())
+const countOutgoingMessage = (exchange: string) => {
+  totalOutgoingMessages.inc({ exchange }, 1)
 }
 
-const countOutgoingError = (exchange: string, routingKey: string) => {
-  failedOutgoingMessages.inc({ exchange, routingKey }, 1, Date.now())
+const countOutgoingError = (exchange: string) => {
+  failedOutgoingMessages.inc({ exchange }, 1)
 }
+
+const waitingDuration = new Histogram({
+  name: 'elementals_rabbitmq_waiting_duration_seconds',
+  help: 'Time that messages spent waiting in the queue before they are picked up by the consumers',
+  labelNames: ['queue'],
+  buckets: [0.1, 0.5, 1, 2, 5, 10, 30, 60, 120, 360, 600, 1800]
+})
+
+const processingDuration = new Histogram({
+  name: 'elementals_rabbitmq_processing_duration_seconds',
+  help: 'Time that consumers spent processing messages from a queue',
+  labelNames: ['queue'],
+  buckets: [0.05, 0.1, 0.25, 0.5, 1, 2, 5, 10, 30, 60]
+})
 
 export interface ChannelConfig {
   inputExchange: string,
@@ -80,10 +100,12 @@ const wrapper = (configName: string): RabbitMQ => {
     const errorExchange = channelConfig.errorExchange
     const errorQueue = `${inputQueue}_errors`
     const prefetch = channelConfig.prefetch ?? 1
+    initializeQueueMetrics(inputQueue)
 
     const onMessage = async (message: ConsumeMessage | null) => {
       if (message !== null) {
-        countIncomingMessage(inputQueue)
+        countIncomingMessage(inputQueue, message.properties)
+        const end = processingDuration.startTimer({ queue: inputQueue })
         try {
           const eventData = JSON.parse(message.content.toString())
           try {
@@ -101,6 +123,8 @@ const wrapper = (configName: string): RabbitMQ => {
           const errorMessage = 'RabbitMQ event processing failed'
           logger.error(errorMessage, message, err)
           channelWrapper.nack(message, false, false)
+        } finally {
+          end()
         }
       }
     }
@@ -139,7 +163,7 @@ const wrapper = (configName: string): RabbitMQ => {
 
   const publish = async (exchange: string, type: string, routingKey: string, data: any, options?: Options.Publish) => {
     try {
-      countOutgoingMessage(exchange, routingKey)
+      countOutgoingMessage(exchange)
       await publisherChannelWrapper.addSetup((channel: ConfirmChannel) => {
         channel.assertExchange(exchange, type)
       })
@@ -149,7 +173,7 @@ const wrapper = (configName: string): RabbitMQ => {
       const context = { body: data, exchange, routingKey }
       logger.info(message, context)
     } catch (err) {
-      countOutgoingError(exchange, routingKey)
+      countOutgoingError(exchange)
       const errorMessage = 'RabbitMQ message publishing failed'
       const context = { body: data, exchange, routingKey }
       logger.error(errorMessage, context, err)
